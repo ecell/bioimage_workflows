b1e510e5066fbc55d9d3b2c44416757ecada842a
Kozo Nishida <kozo.nishida@gmail.com>
2021-06-21 13:35:53 +0900

    Create generation.py

---
+++ code_dir/generation.py
@@ -0,0 +1,58 @@
+seed = args.seed
+interval = args.interval
+num_samples = args.num_samples
+num_frames = args.num_frames
+exposure_time = args.exposure_time
+
+Nm = [100, 100, 100]
+Dm = [0.222e-12, 0.032e-12, 0.008e-12]
+transmat = [
+    [0.0, 0.5, 0.0],
+    [0.5, 0.0, 0.2],
+    [0.0, 1.0, 0.0]]
+
+for key, value in vars(args).items():
+    log_param(key, value)
+
+import tempfile
+artifacts = pathlib.Path(tempfile.mkdtemp()) / "artifacts"
+artifacts.mkdir(parents=True, exist_ok=True)
+
+#XXX: HERE
+
+import numpy
+rng = numpy.random.RandomState(seed)
+
+import scopyon
+
+config = scopyon.DefaultConfiguration()
+config.default.effects.photo_bleaching.switch = False
+config.default.detector.exposure_time = exposure_time
+pixel_length = config.default.detector.pixel_length / config.default.magnification
+L_2 = config.default.detector.image_size[0] * pixel_length * 0.5
+L_2
+
+timepoints = numpy.linspace(0, interval * num_frames, num_frames + 1)
+ndim = 2
+
+config.save(artifacts / 'config.yaml')
+
+for i in range(num_samples):
+    samples = scopyon.sample(timepoints, N=Nm, lower=-L_2, upper=+L_2, ndim=ndim, D=Dm, transmat=transmat, rng=rng)
+    inputs = [(t, numpy.hstack((points[:, : ndim], points[:, [ndim + 1]], numpy.ones((points.shape[0], 1), dtype=numpy.float64)))) for t, points in zip(timepoints, samples)]
+    ret = list(scopyon.generate_images(inputs, num_frames=num_frames, config=config, rng=rng, full_output=True))
+
+    inputs_ = []
+    for t, data in inputs:
+        inputs_.extend(([t] + list(row) for row in data))
+    inputs_ = numpy.array(inputs_)
+    numpy.save(artifacts / f"inputs{i:03d}.npy", inputs_)
+
+    numpy.save(artifacts / f"images{i:03d}.npy", numpy.array([img.as_array() for img, infodict in ret]))
+    ret[0][0].save(artifacts / f"image{i:03d}_000.png")
+
+    true_data = []
+    for t, (_, infodict) in zip(timepoints, ret):
+        true_data.extend([t, key] + list(value) for key, value in infodict['true_data'].items())
+    true_data = numpy.array(true_data)
+    numpy.save(artifacts / f"true_data{i:03d}.npy", true_data)
