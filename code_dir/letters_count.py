d6703058791aad64abe26203993b18642668de54
Manabu Ishii <manabu.ishii.rb@gmail.com>
2021-06-28 13:16:13 +0900

    Update letters_count.py

--- code_dir/letters_count.py
+++ code_dir/letters_count.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 import json
 
-letter1="a"
-letter2="e"
 
-f = open('sample.txt', 'r')
-data = f.read()
-f.close()
+# https://docs.python.org/ja/3/library/typing.html
+# 引数が
+def count_letters(filename :str, letter1 :str, letter2 :str) -> str:
+    f = open(filename, 'r')
+    data = f.read()
+    f.close()
 
-count_letter1 = data.count(letter1)
-count_letter2 = data.count(letter2)
-print(count_letter1)
-print(count_letter2)
-json_str = json.dumps({"letters1_count":count_letter1, "letter2_count":count_letter2})
-print(json_str)
+    count_letter1 = data.count(letter1)
+    count_letter2 = data.count(letter2)
+
+    json_str = json.dumps({"letters1_count":count_letter1, "letter2_count":count_letter2})
+
+    # f = open(outputfilename, 'w')
+    # data = f.write(json_str)
+    # f.close()
+
+    # https://docs.python.org/ja/3/library/typing.html
+    # TODO: ディレクトリを返す
+    return json_str
+
+#json_str = analyze_sample("sample.txt", letter1, letter2)
+#print(json_str)
