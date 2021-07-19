a201f4cfe6b8b4daac3d77390c9061ee865f3428
Manabu Ishii <manabu.ishii.rb@gmail.com>
2021-07-19 13:51:08 +0900

    Update test code

--- code_dir/test_toml-t.py
+++ code_dir/test_toml-t.py
@@ -1,22 +1,25 @@
-from tomlfunc import read_toml, get_inputs
-from workflow import kaizu_generation
+from tomlfunc import read_toml
+from function_list import kaizu_generation
 
 def test_1():
   assert True
 
 def test_read_toml():
-    a = read_toml("./test.toml")
+    a = read_toml("./params.toml")
     assert a != None
     assert a["title"] == "scopyon"
+    print(a["generation"]["inputs"])
     #print(a["generation"])
     assert a["generation"]["inputs"]["num_samples"] == 1
     assert a["generation"]["outputs"]["artifacts"] == "./artifacts"
 
-def test_get_inputs():
-    a = get_inputs("./test.toml")
-    assert a["num_samples"] == 1
-
+# def test_get_inputs():
+#     a = read_toml("./params.toml")
+#     assert a["generation"]["inputs"]["num_samples"] == 1
+#     # a = read_toml("./params.toml")
+#     # assert a["num_samples"] == 1
+    
 def test_kaizu_generation():
-    a = get_inputs("./test.toml")
+    a = read_toml("./params.toml")
     output = kaizu_generation(a)
     assert type(output) == str
