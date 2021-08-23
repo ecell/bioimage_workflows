69305f0a3bf5ad6b149b1d1e74512f4416570fa4
Kazunari Kaizu <kwaizu@gmail.com>
2021-08-23 16:47:32 +0900

    Move already_ran

--- describe_workflow.py
+++ describe_workflow.py
@@ -11,38 +11,6 @@ from mlflow.entities import RunStatus
 from bioimage_workflow.toml import read_toml
 from function_list import kaizu_generation, kaizu_analysis1, kaizu_analysis2
 
-def _already_ran(run_name, parameters, experiment_id=None):
-    """Best-effort detection of if a run with the given entrypoint name,
-    parameters, and experiment id already ran. The run must have completed
-    successfully and have at least the parameters provided.
-    """
-    experiment_id = experiment_id if experiment_id is not None else _get_experiment_id()
-    client = mlflow.tracking.MlflowClient()
-    all_run_infos = reversed(client.list_run_infos(experiment_id))
-    for run_info in all_run_infos:
-        full_run = client.get_run(run_info.run_id)
-        tags = full_run.data.tags
-        if tags.get(mlflow_tags.MLFLOW_RUN_NAME, None) != run_name:
-            continue
-        match_failed = False
-        for param_key, param_value in parameters.items():
-            run_value = full_run.data.params.get(param_key)
-            if str(run_value) != str(param_value):
-                match_failed = True
-                break
-        if match_failed:
-            continue
-
-        if run_info.to_proto().status != RunStatus.FINISHED:
-            eprint(
-                ("Run matched, but is not FINISHED, so skipping " "(run_id=%s, status=%s)")
-                % (run_info.run_id, run_info.status)
-            )
-            continue
-
-        return client.get_run(run_info.run_id)
-    eprint("No matching run has been found.")
-    return None
 
 expr_name = "hoge1"
 mlflow.set_tracking_uri("http://11.11.11.11:1111")
@@ -68,22 +36,6 @@ ana2_inputs = read_toml(tomlpath)["analysis2"]["inputs"]
 # mlflowのrunidを習得できるようにしておく。
 run_name = 'generation'
 
-#run = _already_ran(run_name, gen_inputs)
-
-# if run is None:
-#     # キャッシュがないので、新しく実行する。
-#     with mlflow.start_run(run_name=run_name) as run:
-#         run = mlflow.active_run()
-#         output = kaizu_generation(gen_inputs)
-#         for key, value in gen_inputs.items():
-#             log_param(key, value)
-#         print(output)
-#         # mlflow tracking serverにput
-#         log_artifacts(output.replace("file://", ""))
-#         print(run)
-# else:
-#     print("_already_ran worked!!!")
-
 with mlflow.start_run(run_name=run_name) as run:
     print(mlflow.get_artifact_uri())
     run = mlflow.active_run()
@@ -93,11 +45,6 @@ with mlflow.start_run(run_name=run_name) as run:
     print(output)
     log_artifacts(output.replace("file://", ""))
     print(run)
-
-# if run is None:
-#     print("Something wrong at generation")
-# for key, value in a.items():
-#     log_param(key, value)
 
 ## さきほど取得しておいた、runidをもとに、artifactsを取得するようにする
 
@@ -134,16 +81,3 @@ print("analysis1_run_id=["+analysis1_run_id+"]")
 #analysis1_artifacts_localpath = client.download_artifacts(analysis1_run_id, ".")
 #print("analysis1_artifacts_localpath=["+analysis1_artifacts_localpath+"]")
 #a["artifacts_pathname"] = analysis1_artifacts_localpath
-# run = None
-# with mlflow.start_run(run_name='analysis2') as run:
-#     run = mlflow.active_run()
-#     output = kaizu_analysis2(ana2_inputs, generation_artifacts_localpath, analysis1_artifacts_localpath)
-#     for key, value in ana2_inputs.items():
-#         log_param(key, value)
-#     print(output)
-#     # mlflow tracking serverにput
-#     log_artifacts(output["artifacts"].replace("file://", ""))
-#     print(run)
-
-# if run is None:
-#     print("Something wrong at analysis2")
