b2bd5b7650bf3aa5fcf0c96eeef9d3a8eeec847b
Kazunari Kaizu <kwaizu@gmail.com>
2021-08-26 13:59:58 +0900

    Move functions to utils

--- bioimage_workflow/utils.py
+++ bioimage_workflow/utils.py
@@ -1,5 +1,11 @@
 import tempfile
 import contextlib
+import importlib
+import pathlib
+
+import mlflow
+from mlflow import log_metric, log_param, log_artifacts
+
 
 def mkdtemp_persistent(*args, persistent=True, **kwargs):
     if persistent:
@@ -9,6 +15,61 @@ def mkdtemp_persistent(*args, persistent=True, **kwargs):
         return normal_mkdtemp()
     else:
         return tempfile.TemporaryDirectory(*args, **kwargs)
+
+def get_function(function_path):
+    function_path = function_path.split('.')
+    module = importlib.import_module('.'.join(function_path[: -1]))
+    func = getattr(module, function_path[-1])
+    return func
+
+def download_artifacts(client, run_id, path='', dst_path=None):
+    print(f'run_id = "{run_id}"')
+    dst_path.mkdir()
+    artifacts_path = client.download_artifacts(run_id=run_id, path=path, dst_path=str(dst_path))
+    print("download from Azure worked!!")
+    print(f'artifacts_path = "{artifacts_path}"')
+    return pathlib.Path(artifacts_path)
+
+def run_rule(run_name, config, inputs=(), idx=None, persistent=False, rootpath='.', client=None):
+    assert client is not None or len(inputs) == 0
+
+    target = config[run_name] if idx is None else config[run_name][idx]
+
+    with mlflow.start_run(run_name=run_name) as run:
+        func_name = target["function"]
+        print(f'run_name = "{run_name}", func_name = "{func_name}"')
+        print(mlflow.get_artifact_uri())
+        run = mlflow.active_run()
+        print(f'run_id = "{run.info.run_id}"')
+        params = target["params"]
+        print(f'params = "{params}"')
+        func = get_function(func_name)
+
+        log_param('function', func_name)
+        for i, run_id in enumerate(inputs):
+            log_param(f'inputs{i}', run_id)
+        log_param('output', run.info.run_id)  #XXX: optional
+
+        for key, value in params.items():
+            log_param(key, value)
+
+        with mkdtemp_persistent(persistent=persistent, dir=rootpath) as outname:
+            working_dir = pathlib.Path(outname)
+            input_paths = tuple(download_artifacts(client, run_id, dst_path=working_dir / f'input{i}') for i, run_id in enumerate(inputs))
+            output_path = working_dir / 'output'
+            output_path.mkdir()
+            artifacts, metrics = func(input_paths, output_path, params)
+            print(f'artifacts = "{artifacts}"')
+            print(f'metrics = "{metrics}"')
+            log_artifacts(artifacts.replace("file://", ""))
+            for key, value in metrics.items():
+                log_metric(key, value)
+
+    if run is None:
+        print('Something wrong at "{run_name}"')
+    return run
+
+
 
 # import sys
 # from mlflow import log_metric, log_param, log_artifacts
