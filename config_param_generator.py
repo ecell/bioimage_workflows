ca5e8154b82ef9b08cc9eadced0b4a5eea8d931f
Kozo Nishida <kozo.nishida@gmail.com>
2021-09-07 17:37:53 +0900

    Update config_param_generator.py

--- config_param_generator.py
+++ config_param_generator.py
@@ -4,7 +4,7 @@ dict_toml = toml.load(open('config.toml'))
 # print(dict_toml)
 
 for i in list(range(40, 60, 2)):
-    for j in list(range(0.10, 0.30, 0.02)):
+    for j in [x / 100 for x in range(10, 30, 2)]:
         dict_toml['template']['analysis1']['params']['threshold'] = i
         dict_toml['generation'][0]['params']['interval'] = j
         dict_toml['generation'][0]['params']['exposure_time'] = j
