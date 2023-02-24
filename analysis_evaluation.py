30280e34fb4d908821673b4be25389c7947288cc
Manabu Ishii <manabu.ishii.rb@gmail.com>
2023-02-24 08:46:23 +0000

    Hydra Optuna MLflow config

--- analysis_evaluation.py
+++ analysis_evaluation.py
@@ -18,6 +18,11 @@ from mlflow import MlflowClient
 from bioimage_workflow.utils import check_if_already_ran
 import sys
 
+## hydra
+import hydra
+from omegaconf import DictConfig, OmegaConf
+import hydra.utils
+
 mlflc = MLflowCallback(
     tracking_uri="http://127.0.0.1:5000",
     metric_name="sum of square x_mean and y_mean",
@@ -55,13 +60,18 @@ evaluation_params = {
 exposure_time=0
 num_samples=1
 
-@mlflc.track_in_mlflow()
-def objective(trial):
+trial_number=0
+# @mlflc.track_in_mlflow()
+# def objective(trial):
+@hydra.main(config_path="bioimage_workflow/conf", config_name="config")
+def objective(cfg: DictConfig):
+    global trial_number
+    trial_number = trial_number + 1
     # variables for analysis.
-    global generation_params, analysis_params, evaluation_params, exposure_time, num_samples
-
-    generation_params["exposure_time"] = exposure_time
-    generation_params["num_samples"] = num_samples
+    # global generation_params, analysis_params, evaluation_params, exposure_time, num_samples
+    
+    # generation_params["exposure_time"] = exposure_time
+    # generation_params["num_samples"] = num_samples
 
     client = MlflowClient(tracking_uri="http://127.0.0.1:5000")
     # print("--- cleint")
@@ -77,17 +87,29 @@ def objective(trial):
 
     ## NOTE: ignore_tags is False, so name is not checked, it checks only params
     local_artifacts_dir="./artifacts/"
-    run=check_if_already_ran(client,"", generation_params)
+    # generation_params["exposure_time"] = cfg.experiment.generation.params.exposure_time
+    # generation_params["num_samples"] = cfg.experiment.generation.params.num_samples
+    # run=check_if_already_ran(client,"", generation_params)
+    run=check_if_already_ran(client,"", cfg.experiment.generation.params)
+    # print("hydra param")
+    # for key, value in cfg.experiment.generation.params.items():
+    #     print(key, value)
+    # print("generation_params param")
+    # for key, value in generation_params.items():
+    #     print(key, value)
+
+    # sys.exit()
+
     if run is None:
         # Exec generation
-        with mlflow.start_run(nested=True, run_name="generation_"+str(trial.number)) as run_analysis:
+        with mlflow.start_run(nested=True, run_name="generation_"+str(trial_number)) as run_analysis:
             generation_output=Path(local_artifacts_dir+'/'+run_analysis.info.run_id)
             print(str(generation_output))
             if not generation_output.exists():
                 generation_output.mkdir(parents=True, exist_ok=True)
 
-            artifacts,metrics = generation1([], generation_output, generation_params)
-            for key, value in generation_params.items():
+            artifacts,metrics = generation1([], generation_output, cfg.experiment.generation.params)
+            for key, value in cfg.experiment.generation.params.items():
                 mlflow.log_param(key, value)
             #
             mlflow.log_artifacts(str(generation_output))
@@ -107,52 +129,54 @@ def objective(trial):
     # input
     # generation_output=Path('./outputs_generation')
     # analysis output
-    # create new dir, use trial.number
-    analysis_output=Path('./outputs_analysis_run/'+str(trial.number))
+    # create new dir, use trial_number
+    analysis_output=Path('./outputs_analysis_run/'+str(trial_number))
     analysis_output.mkdir(parents=True, exist_ok=True)
-    overlap = trial.suggest_float("overlap", 0, 1)
-    threshold = trial.suggest_float("threshold", 30, 70)
+    # overlap = trial.suggest_float("overlap", 0, 1)
+    # threshold = trial.suggest_float("threshold", 30, 70)
 
-    with mlflow.start_run(nested=True, run_name="analysis_"+str(trial.number)) as run_analysis:
-        # set param
-        analysis_params["overlap"]=overlap
-        analysis_params["threshold"]=threshold
+    with mlflow.start_run(nested=True, run_name="evaluation_"+str(trial_number)) as run_evaluation:
+
+        with mlflow.start_run(nested=True, run_name="analysis_"+str(trial_number)) as run_analysis:
+            # set param
+            # analysis_params["overlap"]=overlap
+            # analysis_params["threshold"]=threshold
+            
+            a,b = analysis1([generation_output], analysis_output, cfg.experiment.analysis.params)
+            num_spots = b["num_spots"]
+            # Set param
+            mlflow.log_param("overlap", cfg.experiment.analysis.params.overlap)
+            mlflow.log_param("threshold", cfg.experiment.analysis.params.threshold)
+
+            # Set metric
+            mlflow.log_metric("num_spots", num_spots)
+            # End analysis run
+            # mlflow.end_run()
+    
+        ## call evaluation
+        # output
+        evaluation_output=Path('./outputs_evaluation_run/'+str(trial_number))
+        evaluation_output.mkdir(parents=True, exist_ok=True)
+
+        # max_distance = trial.suggest_float("max_distance", 0, 1)
+        max_distance = evaluation_params["max_distance"]
+
+        mlflow.log_param("overlap", cfg.experiment.analysis.params.overlap)
+        mlflow.log_param("threshold", cfg.experiment.analysis.params.threshold)
+
+        mlflow.log_param("max_distance", cfg.experiment.evaluation.params.max_distance)
         
-        a,b = analysis1([generation_output], analysis_output, analysis_params)
-        num_spots = b["num_spots"]
-        # Set param
-        mlflow.log_param("overlap", overlap)
-        mlflow.log_param("threshold", threshold)
+        # evaluation_params["max_distance"] = max_distance
+        c,d = evaluation1([generation_output,analysis_output], evaluation_output, cfg.experiment.evaluation.params)
 
-        # Set metric
-        mlflow.log_metric("num_spots", num_spots)
-        # End analysis run
-        # mlflow.end_run()
-    
-    ## call evaluation
-    # output
-    evaluation_output=Path('./outputs_evaluation_run/'+str(trial.number))
-    evaluation_output.mkdir(parents=True, exist_ok=True)
+        x_mean = d["x_mean"]
+        y_mean = d["y_mean"]
 
-    # max_distance = trial.suggest_float("max_distance", 0, 1)
-    max_distance = evaluation_params["max_distance"]
+        mlflow.log_metric("x_mean", x_mean)
+        mlflow.log_metric("y_mean", y_mean)
 
-    mlflow.log_param("overlap", overlap)
-    mlflow.log_param("threshold", threshold)
-
-    mlflow.log_param("max_distance", max_distance)
-    
-    evaluation_params["max_distance"] = max_distance
-    c,d = evaluation1([generation_output,analysis_output], evaluation_output, evaluation_params)
-
-    x_mean = d["x_mean"]
-    y_mean = d["y_mean"]
-
-    mlflow.log_metric("x_mean", x_mean)
-    mlflow.log_metric("y_mean", y_mean)
-
-    # TODO: not hard code 600
-    result = (x_mean)**2+(y_mean)**2
+        # TODO: not hard code 600
+        result = (x_mean)**2+(y_mean)**2
     return result
 
 
@@ -169,16 +193,18 @@ def main():
     # print(a)
     # print(b)
     # Execute Optuna MLFlow
-    study = optuna.create_study(storage="sqlite:///example2.db", study_name="test_x_y_mean_storage_6", load_if_exists=True, sampler=optuna.samplers.CmaEsSampler())
+    # study = optuna.create_study(storage="sqlite:///example2.10.1.db", study_name="test_x_y_mean_storage_7_2.10.1", load_if_exists=True, sampler=optuna.samplers.CmaEsSampler())
+    objective()
+
     # print("--- study")
     # print(dir(study))
-    for j in range(1,6):
-        num_samples = j
-        exposure_time=0
-        for i in range(5):
-            exposure_time=exposure_time+0.033
-            study.optimize(objective, n_trials=5, callbacks=[mlflc])
-            print(study.best_params)
+    # for j in range(1,6):
+    #     num_samples = j
+    #     exposure_time=0
+    #     for i in range(5):
+    #         exposure_time=exposure_time+0.033
+    #         study.optimize(objective, n_trials=5, callbacks=[mlflc])
+    #         print(study.best_params)
 
     # ここで別のexperimentにoptuna記録するという方法もある。
 
